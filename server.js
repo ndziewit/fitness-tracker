d152d901f896dfb7ba5e48f4d663c07ced6c12ee
Sweat <upsidedownplatypus@gmail.com>
2020-10-06 17:55:28 -0400

    connected mongo to heroku

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workouts',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+     process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+     {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
+     }
+   );
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
