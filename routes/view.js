e86a92bb4334f5344b07b182e7585bb04d797065
Sweat <upsidedownplatypus@gmail.com>
2020-10-06 15:18:42 -0400

    added routes

---
+++ routes/view.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const path = require("path");
+//const db = require("mongoose");
+
+router.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
