01b6aaff42b44ad53ac2eb49eac3358036fa0094
Sweat <upsidedownplatypus@gmail.com>
2020-10-27 17:33:16 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(logger("dev"));
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-     process.env.MONGODB_URI || 'mongodb://localhost/fitnesstracker',
+     process.env.MONGODB_URI || 'mongodb://localhost/workout',
      {
        useNewUrlParser: true,
        useUnifiedTopology: true,
